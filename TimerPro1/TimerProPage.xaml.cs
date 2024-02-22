b6999f7897d0632066a63819eaa912da98742d23
500162442 <500162442@fvtc.edu>
2024-02-21 19:12:04 -0800

    Initial check-in of module TimerPro1

---
+++ TimerPro1/TimerProPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using TimerPro1.custom;
+
+using Xamarin.Forms;
+
+namespace TimerPro1
+{
+    
+    public partial class TimerProPage : ContentPage
+    {
+        TimerLogic oTimerLogic;
+        bool isRunning;
+        public TimerProPage()
+        {
+            InitializeComponent();
+            oTimerLogic = new TimerLogic();
+        }
+
+        void Start_Clicked(System.Object sender, System.EventArgs e)
+        {
+            btnStart.IsEnabled = false;
+            btnStop.IsEnabled = true;
+            isRunning = true;
+
+            Device.StartTimer(TimeSpan.FromSeconds(1), () =>{
+                if (isRunning)
+                {
+                    oTimerLogic.SetTickCount();
+                    lblDisplay.Text = oTimerLogic.GetFormattedString();
+                }
+                
+                return isRunning;
+            });
+
+        }
+
+        void Stop_Clicked(System.Object sender, System.EventArgs e)
+        {
+            btnStart.IsEnabled = true;
+            btnStop.IsEnabled = false;
+            isRunning = false;
+        }
+
+        void Reset_Clicked(System.Object sender, System.EventArgs e)
+        {
+            oTimerLogic.Reset();
+            lblDisplay.Text = oTimerLogic.GetFormattedString();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            btnStart.IsEnabled = true;
+            btnStop.IsEnabled = false;
+            lblDisplay.Text = "00:00:00";
+        }
+    }
+}
